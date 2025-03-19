c179919e76366764f5de0819b5a09be55aed0b61
Umesh Kashyap <umesh.kashyap3729@gmail.com>
2025-03-19 14:45:18 +0530

    basic server and file structure up and running

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import bodyParser from "body-parser";
 const app = new express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
-})
+});
